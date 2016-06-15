ed5b3c6b11b446ccb8f4f547be4c071a14ebc868
vortex610 <vortexuelpm@gmail.com>
2016-06-15 16:46:43 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,45 +5,42 @@ fuel environment create --name Perf-1 --release 2
 ID=`fuel env | awk '/Perf-1/ {print $1}'`
 while [[ "$verify" != "  vlan_start: 101" ]]
 do
-	fuel --env $ID network download
-	mv network_$ID.yaml network.yaml
-	verify=`awk '(NR == 70)' network.yaml`
-	echo "$verify"
+        fuel --env $ID network download
+        mv network_$ID.yaml network.yaml
+        verify=`awk '(NR == 70)' network.yaml`
+        if [[ "$verify" != "  vlan_start: 101" ]];then echo "wrong file, downloading again";fi
 done
-echo "$verify"
+echo "network OK"
 sleep 1;
 curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/network_diff.patch' | patch -b -d /root/ -p1
 mv network.yaml network_$ID.yaml
 fuel --env $ID network upload
 
 i=0
-stop=5
+stop=60
 cnt=0
 while [[ $i != $stop ]];
 do
-	a=`fuel node | awk '/discover/ {print $1}'`
-	for item in ${a[@]};
-	do
-		cnt=$[cnt+1];
-	done
-	if [[ -n $a ]];
-	then
+        a=`fuel node | awk '/discover/ {print $1}'`
+        for item in ${a[@]};do cnt=$[cnt+1]; done
+        if [[ $cnt == "4" ]];then
                 i=$stop
                 echo "Found $cnt nodes"
         else
                 i=$[i+1]
-                if [[ $i == $stop ]];
-                then
-                	echo "No nodes discovered for $[$i*60] sec"
+                if [[ $i == $stop ]];then
+                        echo "No nodes discovered for $[$i*10] sec"
                 else
-                	echo "waiting for nodes"
-                	sleep 60
+                        echo "waiting for nodes 10 sec"
+                        echo "found $cnt nodes"
+                        sleep 10
                 fi
-	fi
+        fi
+        cnt=0
 done
 ips=`fuel node | awk '/0c:c4:7a:0c:/ {print $9}'`
 for nod in ${ips[@]};do
-	ssh $nod 'ip link set up dev enp2s0f0; ip link set up dev enp2s0f1';
+        ssh $nod 'ip link set up dev enp2s0f0; ip link set up dev enp2s0f1';
 done
 ID_CONTROLLER=`fuel node | awk '/discover/ {print $1}' | head -n 1`
 ID_OTHER=`fuel node | awk '/discover/ {print $1}' | sed '1d'`
@@ -67,10 +64,10 @@ mv /root/deployment/controller.yaml /root/deployment_$ID/$ID_CONTROLLER.yaml
 
 for item in $ID_OTHER;
 do
-	mv /root/deployment_$ID/$item.yaml /root/deployment/compute.yaml
-	curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/COMPUTE.patch' > compute_$item.patch
-	patch -p1 /root/deployment/compute.yaml compute_$item.patch
-	mv /root/deployment/compute.yaml /root/deployment_$ID/$item.yaml
+        mv /root/deployment_$ID/$item.yaml /root/deployment/compute.yaml
+        curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/COMPUTE.patch' > compute_$item.patch
+        patch -p1 /root/deployment/compute.yaml compute_$item.patch
+        mv /root/deployment/compute.yaml /root/deployment_$ID/$item.yaml
 done
 fuel --env $ID deployment upload
 #fuel --env $ID deploy-changes

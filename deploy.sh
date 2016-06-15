c9a409db5b7888c1ca590d8776d2ad82efc016ed
vortex610 <vortexuelpm@gmail.com>
2016-06-15 15:22:55 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -59,13 +59,15 @@ fuel --env $ID settings download
 fuel --env $ID deployment default
 
 mv /root/deployment_$ID/$ID_CONTROLLER.yaml /root/deployment/controller.yaml
-curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/CONTROLLER.patch' | patch -n /root/ -p1
+curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/CONTROLLER.patch' > controller_$ID_CONTROLLER.patch
+patch -p1 /root/deployment/controller.yaml controller_$ID_CONTROLLER.patch
 mv /root/deployment/controller.yaml /root/deployment_$ID/$ID_CONTROLLER.yaml
 
 for item in $ID_OTHER;
 do
 	mv /root/deployment_$ID/$item.yaml /root/deployment/compute.yaml
-	curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/COMPUTE.patch' | patch -n /root/ -p1
+	curl -s 'https://raw.githubusercontent.com/vortex610/deploy/master/VLAN_bond_DVR_OFF/Perf-1/1/COMPUTE.patch' > compute_$item.patch
+	patch -p1 /root/deployment/compute.yaml compute_$item.patch
 	mv /root/deployment/compute.yaml /root/deployment_$ID/$item.yaml
 done
 fuel --env $ID deployment upload
